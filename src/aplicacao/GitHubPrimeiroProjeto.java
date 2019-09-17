706c49640c9e7e5562a21a5f9f1f5d1bcaa39efc
Andre Silva Bender <asbender4464@gmail.com>
2019-09-17 19:59:48 -0300

    Primeiro projeto criado

---
+++ src/aplicacao/GitHubPrimeiroProjeto.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+public class GitHubPrimeiroProjeto {
+
+	public static void main(String[] args) {
+
+		System.out.println("Alô Mundo! Meu primeiro projeto no Git Hub!");
+
+	}
+
+}
