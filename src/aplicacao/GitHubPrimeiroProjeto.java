ff1f0788e8963ecb8eb59a96c31a0e94108d2116
Andre Silva Bender <asbender4464@gmail.com>
2019-09-18 10:29:09 -0300

    Salvando a 2a versão.

--- src/aplicacao/GitHubPrimeiroProjeto.java
+++ src/aplicacao/GitHubPrimeiroProjeto.java
@@ -5,6 +5,7 @@ public class GitHubPrimeiroProjeto {
 	public static void main(String[] args) {
 
 		System.out.println("Alô Mundo! Meu primeiro projeto no Git Hub!");
+		System.out.println("Esta linha foi inserida para fazer o 2o commit no GitHub!");
 
 	}
 
