8091150913d687fcad3072bbab86d13d656838f0
Andre Silva Bender <asbender4464@gmail.com>
2019-09-18 11:22:58 -0300

    Este é o 4o commit

--- src/aplicacao/GitHubPrimeiroProjeto.java
+++ src/aplicacao/GitHubPrimeiroProjeto.java
@@ -7,6 +7,7 @@ public class GitHubPrimeiroProjeto {
 		System.out.println("Alô Mundo! Meu primeiro projeto no Git Hub!");
 		System.out.println("Esta linha foi inserida para fazer o 2o commit no GitHub!");
 		System.out.println("Este será o terceiro commit.");
+		System.out.println("Este será o quarto commit.");
 
 	}
 
